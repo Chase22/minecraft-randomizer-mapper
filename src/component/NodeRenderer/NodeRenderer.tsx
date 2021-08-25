5446d0eed493b524a76a6654f1804f13355cfbf2
Chase22 <chase@mailbox.org>
2021-08-25 20:28:57 +0200

    Only Render Nodes on change

--- src/component/NodeRenderer/NodeRenderer.tsx
+++ src/component/NodeRenderer/NodeRenderer.tsx
@@ -1,8 +1,9 @@
-import {ArrowHeadType, Edge, isNode, Node, Position, ReactFlowProvider} from "react-flow-renderer";
+import {ArrowHeadType, Edge, Elements, isNode, Node, Position, ReactFlowProvider} from "react-flow-renderer";
 import LayoutFlow from "./Layout-Flow";
 import {ItemConnection} from "../../util/ItemConnection";
 import dagre from "dagre";
-import React from "react";
+import React, {useState} from "react";
+import useDeepCompareEffect from "use-deep-compare-effect";
 
 interface NodeRendererProps {
   connections: ItemConnection[]
@@ -15,67 +16,73 @@ const dagreGraph = new dagre.graphlib.Graph();
 dagreGraph.setDefaultEdgeLabel(() => ({}));
 
 const NodeRenderer: React.FC<NodeRendererProps> = ({connections, setOnFocusIdHandler, sources, targets}) => {
+  const [layoutedElements, setLayoutedElements] = useState<Elements>();
 
-  const nodes: Node[] = Array.from(new Set([...sources, ...targets])).map(name => {
-      let type = 'default';
-      if (sources.includes(name) && !targets.includes(name)) {
-        type = 'input'
-      } else if (!sources.includes(name) && targets.includes(name)) {
-        type = 'output'
+  useDeepCompareEffect(() => {
+    const nodes: Node[] = Array.from(new Set([...sources, ...targets])).map(name => {
+        let type = 'default';
+        if (sources.includes(name) && !targets.includes(name)) {
+          type = 'input'
+        } else if (!sources.includes(name) && targets.includes(name)) {
+          type = 'output'
+        }
+
+        return {
+          id: name,
+          type,
+          data: {label: name},
+          position: {x: 0, y: 0},
+        }
       }
+    )
 
+    const edges: Edge[] = connections.map(connection => {
+      if (!connection.target) {
+        return null
+      }
       return {
-        id: name,
-        type,
-        data: {label: name},
-        position: {x: 0, y: 0},
+        id: `${connection.source}-${connection.target}`,
+        source: connection.source,
+        target: connection.target,
+        type: 'smoothstep',
+        arrowHeadType: ArrowHeadType.ArrowClosed
+
       }
-    }
-  )
+    }).filter(value => value !== null) as Edge[]
 
-  const edges: Edge[] = connections.map(connection => {
-    if (!connection.target) {
-      return null
-    }
-    return {
-      id: `${connection.source}-${connection.target}`,
-      source: connection.source,
-      target: connection.target,
-      type: 'smoothstep',
-      arrowHeadType: ArrowHeadType.ArrowClosed
+    dagreGraph.setGraph({rankdir: 'LR'});
 
-    }
-  }).filter(value => value !== null) as Edge[]
+    const elements = [...nodes, ...edges]
 
-  dagreGraph.setGraph({rankdir: 'LR'});
+    elements.forEach((el) => {
+      if (isNode(el)) {
+        dagreGraph.setNode(el.id, {width: 150, height: 50});
+      } else {
+        dagreGraph.setEdge(el.source, el.target);
+      }
+    });
+    dagre.layout(dagreGraph);
 
-  const elements = [...nodes, ...edges]
+    setLayoutedElements(elements.map((el) => {
+      if (isNode(el)) {
+        const nodeWithPosition = dagreGraph.node(el.id);
+        el.targetPosition = Position.Left;
+        el.sourcePosition = Position.Right;
+        // we need to pass a slighltiy different position in order to notify react flow about the change
+        // @TODO how can we change the position handling so that we dont need this hack?
+        el.position = {x: nodeWithPosition.x + Math.random() / 1000, y: nodeWithPosition.y};
+      }
 
-  elements.forEach((el) => {
-    if (isNode(el)) {
-      dagreGraph.setNode(el.id, {width: 150, height: 50});
-    } else {
-      dagreGraph.setEdge(el.source, el.target);
-    }
-  });
-  dagre.layout(dagreGraph);
+      return el;
+    }));
+  }, [connections, sources, targets])
 
-  const layoutedElements = elements.map((el) => {
-    if (isNode(el)) {
-      const nodeWithPosition = dagreGraph.node(el.id);
-      el.targetPosition = Position.Left;
-      el.sourcePosition = Position.Right;
-      // we need to pass a slighltiy different position in order to notify react flow about the change
-      // @TODO how can we change the position handling so that we dont need this hack?
-      el.position = {x: nodeWithPosition.x + Math.random() / 1000, y: nodeWithPosition.y};
-    }
-
-    return el;
-  });
 
   return (
     <ReactFlowProvider>
-      <LayoutFlow elements={layoutedElements} setOnFocusIdHandler={setOnFocusIdHandler} />
+      {layoutedElements &&
+      <LayoutFlow elements={layoutedElements} setOnFocusIdHandler={setOnFocusIdHandler}/>
+      }
     </ReactFlowProvider>
   )
 }
